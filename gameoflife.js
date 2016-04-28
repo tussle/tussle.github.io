c8f5d37c0766b9c94718d8ea87fff2e17a993de0
tussle <gaming124@gmail.com>
2016-04-28 17:45:17 +0100

    adding a start to game of life

---
+++ gameoflife.js
@@ -0,0 +1,22 @@
+function Game(canvas){
+
+this.canvas = canvas
+
+this.size_x = 10;
+this.size_y = 10;
+
+this.grid = new Array(this.size_y).fill(new Array(this.size_x).fill(0));
+
+this.draw_grid = function(){
+var c = this.canvas
+var ctx = c.getContext("2d");
+
+for (var x = 0; x <= this.size_x; x++) {
+    ctx.moveTo(0.5 + x*10, 10);
+    ctx.lineTo(0.5 + x*10, this.size_y*10);
+}
+
+ctx.strokeStyle = "black";
+ctx.stroke();
+};
+}
