2cfc1c7ab6d04913a236f665d1154f42db3a13a3
tussle <gaming124@gmail.com>
2016-05-01 00:03:29 +0100

    quick update

--- gameoflife.js
+++ gameoflife.js
@@ -26,7 +26,7 @@ this.grid[5][5] = 1
 for( var x = 0; x < this.size_x; x++){
     for(var y = 0; y< this.size_y; y++){
 	    if(this.grid[x][y] == 1){
-	        ctx.fillRect(50 , 50,51,51)//0.5 + x*10 +this.size_x , 0.5 + y*10 +this.size_y );
+	        ctx.fillRect(0 ,0,1,1)//0.5 + x*10 +this.size_x , 0.5 + y*10 +this.size_y );
 		}
 	}
 }
