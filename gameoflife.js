78b39d538e0e16648877a73184003f2191b2ab2b
tussle <gaming124@gmail.com>
2016-05-07 13:19:10 +0100

    another commit

--- gameoflife.js
+++ gameoflife.js
@@ -26,7 +26,7 @@ this.grid[5][5] = 1
 for( var x = 0; x < this.size_x; x++){
     for(var y = 0; y< this.size_y; y++){
 	    if(this.grid[x][y] == 1){
-	        ctx.fillRect(0.5 ,0.5,10,10)//0.5 + x*10 +this.size_x , 0.5 + y*10 +this.size_y );
+	        ctx.fillRect(0.5 + (x+1)*10 , 0.5+ (y+1)*10 , 0.5 + (x+2)*10 , 0.5+ (y+2)*10)//0.5 + x*10 +this.size_x , 0.5 + y*10 +this.size_y );
 		}
 	}
 }
