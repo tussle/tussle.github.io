2ce6c6a9fc83c87d2c55c976b6967a5a4bea172b
tussle <gaming124@gmail.com>
2016-05-07 13:14:26 +0100

    another commit

--- gameoflife.js
+++ gameoflife.js
@@ -11,12 +11,12 @@ this.draw_grid = function(){
 var c = this.canvas
 var ctx = c.getContext("2d");
 
-for (var x = 0; x <= this.size_x; x++) {
+for (var x = 1; x <= this.size_x; x++) {
     ctx.moveTo(0.5 + x*10, 10);
     ctx.lineTo(0.5 + x*10, this.size_y*10);
 }
 
-for (var y = 0; y <= this.size_y; y++) {
+for (var y = 1; y <= this.size_y; y++) {
     ctx.moveTo(10 , 0.5 + y*10);
     ctx.lineTo( this.size_x*10 , 0.5 + y*10);
 }
