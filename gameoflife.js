feb2e72b4cad3d0aa1e5cff95b1764854eba4375
tussle <gaming124@gmail.com>
2016-05-07 14:32:14 +0100

    another commit

--- gameoflife.js
+++ gameoflife.js
@@ -8,9 +8,11 @@ this.size_y = 10;
 this.square_side = 10;
 
 this.grid = new Array(this.size_y).fill(new Array(this.size_x).fill(0));
+this.grid = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
 
 this.draw_grid = function(){
-var c = this.canvas
+var c = this.canvas;
 var ctx = c.getContext("2d");
 
 for (var x = 0; x <= this.size_x; x++) {
@@ -25,14 +27,14 @@ for (var y = 0; y <= this.size_y; y++) {
 //this.grid[4][4] = 1
 for( var x = 0; x < this.size_x; x++){
     for(var y = 0; y< this.size_y; y++){
-	    console.log("x = " + x + " y = " + y + "grid "+ this.grid[x][y])
+	    console.log("x = " + x + " y = " + y + "grid "+ this.grid[x][y]);
 	}
 }
 this.grid[5][5] = 1;
 
 for( var x = 0; x < this.size_x; x++){
     for(var y = 0; y< this.size_y; y++){
-	    console.log("x = " + x + " y = " + y + "grid "+ this.grid[x][y])
+	    console.log("x = " + x + " y = " + y + "grid "+ this.grid[x][y]);
 	}
 }
 //this.grid[6][6] = 1
@@ -40,9 +42,9 @@ for( var x = 0; x < this.size_x; x++){
     for(var y = 0; y< this.size_y; y++){
 	    if(this.grid[x][y] == 1){
 		    console.log("x = " + x + " y = " + y)
-		    var xstart = .5 + this.size_x * x 
-			var ystart= .5 + this.size_y * y 
-	        ctx.fillRect( xstart , ystart , this.square_side, this.square_side)//0.5 + x*10 +this.size_x , 0.5 + y*10 +this.size_y );
+		    var xstart = .5 + this.size_x * x ;
+			var ystart= .5 + this.size_y * y ;
+	        ctx.fillRect( xstart , ystart , this.square_side, this.square_side);//0.5 + x*10 +this.size_x , 0.5 + y*10 +this.size_y );
 		}
 	}
 }
