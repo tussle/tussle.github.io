ac93a862b9838312ac08819c46f4db43bcae9766
tussle <gaming124@gmail.com>
2016-05-07 13:28:39 +0100

    another commit

--- gameoflife.js
+++ gameoflife.js
@@ -5,20 +5,22 @@ this.canvas = canvas
 this.size_x = 10;
 this.size_y = 10;
 
+this.square_side = 10;
+
 this.grid = new Array(this.size_y).fill(new Array(this.size_x).fill(0));
 
 this.draw_grid = function(){
 var c = this.canvas
 var ctx = c.getContext("2d");
 
-for (var x = 1; x <= this.size_x; x++) {
-    ctx.moveTo(0.5 + x*10, 10);
-    ctx.lineTo(0.5 + x*10, this.size_y*10);
+for (var x = 0; x <= this.size_x; x++) {
+    ctx.moveTo(0.5 + x*this.square_side , .5);
+    ctx.lineTo(0.5 + x*this.square_side , .5 + this.size_y*this.square_side );
 }
 
-for (var y = 1; y <= this.size_y; y++) {
-    ctx.moveTo(10 , 0.5 + y*10);
-    ctx.lineTo( this.size_x*10 , 0.5 + y*10);
+for (var y = 0; y <= this.size_y; y++) {
+    ctx.moveTo(.5 , 0.5 + y*this.square_side );
+    ctx.lineTo( .5 + this.size_x*this.square_side  , 0.5 + y*this.square_side );
 }
 //this.grid[4][4] = 1
 this.grid[5][5] = 1
@@ -26,7 +28,7 @@ this.grid[5][5] = 1
 for( var x = 0; x < this.size_x; x++){
     for(var y = 0; y< this.size_y; y++){
 	    if(this.grid[x][y] == 1){
-	        ctx.fillRect(0.5 + (x+1)*10 , 0.5+ (y+1)*10 , 10.5 + (x+1)*10 , 10.5+ (y+1)*10)//0.5 + x*10 +this.size_x , 0.5 + y*10 +this.size_y );
+	        //ctx.fillRect(0.5 + (x+1)*10 , 0.5+ (y+1)*10 , 10.5 + (x+1)*10 , 10.5+ (y+1)*10)//0.5 + x*10 +this.size_x , 0.5 + y*10 +this.size_y );
 		}
 	}
 }
