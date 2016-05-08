1ccd33f641f471e6da51cc5c7d45fc31b0bac978
tussle <gaming124@gmail.com>
2016-05-08 14:32:51 +0100

    another commit

--- gameoflife.js
+++ gameoflife.js
@@ -31,11 +31,7 @@ for (var y = 0; y <= this.size_y; y++) {
     ctx.moveTo(.5 , 0.5 + y*this.square_side );
     ctx.lineTo( .5 + this.size_x*this.square_side  , 0.5 + y*this.square_side );
 }
-this.grid[4][4] = 1
 
-this.grid[5][5] = 1;
-
-this.grid[6][6] = 1
 for( var x = 0; x < this.size_x; x++){
     for(var y = 0; y< this.size_y; y++){
 	    if(this.grid[x][y] == 1){
