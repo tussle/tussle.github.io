218c99e9dff3e2f5e1b98ae52e5f4eff3483cf04
tussle <gaming124@gmail.com>
2016-09-11 14:43:31 +0100

    adding Lights out game

--- LightsOut.js
+++ LightsOut.js
@@ -1,9 +1,9 @@
-function Game(canvas){
+function LightsOutGame(canvas){
 
 this.canvas = canvas
 
-this.size_x = 10;
-this.size_y = 10;
+this.size_x = 5;
+this.size_y = 5;
 
 this.square_side = 10;
 
@@ -48,9 +48,7 @@ ctx.stroke();
 };
 
 this.reset_grid = function(){
-this.grid[4][4] = 1
-this.grid[5][5] = 1
-this.grid[6][6] = 1
+
 }
 
 this.next = function(){
@@ -89,43 +87,15 @@ this.get_neighbours = function(x,y){
     return [neighboursx,neighboursy]
 }
 
-this.living_neighbours = function(x,y){
-    var count = 0;
-	console.log("x = "+x +" y = "+y +" " +this.get_neighbours(x,y))
-	var neighbours = this.get_neighbours(x,y)
-	var neighboursx = neighbours[0];
-	var neighboursy = neighbours[1];
-	for(var i = 0; i < neighboursx.length; i++ ){
-	    
-	    if(this.grid[ neighboursx[i] ][ neighboursy[i] ] == 1){
-		
-		    count++;
-		}
-	}
-	return count;
-}
-this.survives = function(x,y){
-    var count = this.living_neighbours(x,y);
-	return  count <= 3 && count >1 ? true:false;
-}
-
-this.is_born = function(x,y){
-    var count = this.living_neighbours(x,y);
-	return  count == 3 ? true:false;
-}
-
-this.play = function(){
-//TODO continuous updates
-}
-
-this.set_shape = function(shape){
-//TODO add various shape defintions from drop down menu on index page
-}
 
 this.change = function(x,y){
 	x = Math.floor(x/this.size_x);
 	y= Math.floor(y/this.size_y);
+	neighbours = this.get_neighbours(x,y)
 	this.grid[x][y] = this.grid[x][y] == 0? 1:0;
+	for(i=0; i < neighbours.length; i++){
+	    this.grid[negihbours[0][i]][neighbours[1][i]] = this.grid[negihbours[0][i]][neighbours[1][i]] == 0? 1:0;
+	}
 	this.draw_grid();
 }
 }
